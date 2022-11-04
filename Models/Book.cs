babab41ce8b2b958418164cecc2d9e67e5bfae5e
anastasiyaSG <57772057+anastasiyaSG@users.noreply.github.com>
2022-11-04 09:25:25 +0200

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagerTests.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+
+namespace LibraryManagerTests.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+   
+}
---
+++ Models/Response.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace LibraryManagerTests.Models
+{
+    public class Response<T>
+    {
+        public T Payload { get; set; }
+        public Error Error { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+  
+    }
+}
